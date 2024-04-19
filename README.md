4baf2991193c4169224ff9fd2e4fbc56c76d0816
ola <ola.0608@gmail.com>
2024-04-19 19:25:42 +0200

    project publish

---
+++ README.md
@@ -0,0 +1,19 @@
+#### Setup
+
+```sh
+
+npm install
+```
+
+```sh
+npm run dev
+```
+
+#### Global Context and useReducer
+
+This project uses a global context and a useReducer hook to manage the state.
+
+#### Fetch Data
+
+Course API is accessed with Fetch.
+
---
+++ src/reducer.jsx
@@ -0,0 +1,43 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
+
+export const reducer = (state, action) => {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: new Map() };
+  } else if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+    return { ...state, cart: newCart };
+  }else if (action.type === INCREASE) {
+    const newCart = new Map(state.cart);
+    const itemId = action.payload.id
+    const item = newCart.get(itemId)
+    const newItem = {...item, amount: item.amount+1}
+    newCart.set(itemId, newItem)
+    return { ...state, cart: newCart };}  
+    else if (action.type === DECREASE) {
+        const newCart = new Map(state.cart);
+        const itemId = action.payload.id
+        const item = newCart.get(itemId)
+        if(item.amount === 1){
+            newCart.delete(itemId)
+            return {...state, cart: newCart}
+        }
+        const newItem = {...item, amount: item.amount-1}
+        newCart.set(itemId, newItem)
+        return { ...state, cart: newCart };}
+        else if (action.type === LOADING) {
+          return {...state, loading: true}
+        }
+        else if (action.type === DISPLAY_ITEMS) {
+          const newCart = new Map(action.payload.cart.map((item) => [item.id, item]))
+          return {...state, cart: newCart, loading: false}
+        }
+  throw new Error(`no matching action type : ${action.type}`);
+};
